e94a77e5aed86d908f645b4ca462f1d2d675d08d
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-16 21:43:17 +0000

    Reapproach the test strategy
    In light of the refactoring of this project the old tests aren't
    particularly useful or valid, this replaces them with a new suite.

---
+++ spec/lib/openapi_parser/nodes/license/factory_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "openapi_parser/nodes/license/factory"
+require "openapi_parser/nodes/license"
+
+require "support/node_object_factory"
+require "support/helpers/context"
+
+
+RSpec.describe OpenapiParser::Nodes::License::Factory do
+  include Helpers::Context
+
+  it_behaves_like "node object factory", OpenapiParser::Nodes::License do
+    let(:input) do
+      {
+        "name" => "License"
+      }
+    end
+
+    let(:context) { create_context(input) }
+  end
+end
