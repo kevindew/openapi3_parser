a10f9a12b191af63bf2bbb9ae5bc2ae034b4f3c9
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-17 22:27:52 +0000

    ServerVariable node

---
+++ spec/lib/openapi_parser/node_factories/server_variable_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "openapi_parser/node_factories/server_variable"
+require "openapi_parser/nodes/server_variable"
+
+require "support/node_object_factory"
+require "support/helpers/context"
+
+RSpec.describe OpenapiParser::NodeFactories::ServerVariable do
+  include Helpers::Context
+
+  it_behaves_like "node object factory",
+                  OpenapiParser::Nodes::ServerVariable do
+    let(:input) do
+      {
+        "enum" => %w[8443 443],
+        "default" => "8443"
+      }
+    end
+
+    let(:context) { create_context(input) }
+  end
+end
