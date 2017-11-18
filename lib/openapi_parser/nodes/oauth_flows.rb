bb8121186d5db774097d7b8ae910fddc26917f98
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-18 15:47:08 +0000

    Add OauthFlows Node
    This uses a reference in the test, since these are supported and this is
    the first test since adding in references.

--- lib/openapi_parser/nodes/oauth_flows.rb
+++ lib/openapi_parser/nodes/oauth_flows.rb
@@ -1,40 +1,26 @@
 # frozen_string_literal: true
 
-require "openapi_parser/node"
-require "openapi_parser/nodes/oauth_flow"
+require "openapi_parser/node/object"
 
 module OpenapiParser
   module Nodes
     class OauthFlows
-      include Node
-
-      allow_extensions
-
-      field "implicit", input_type: Hash, build: :build_oauth_flow
-      field "password", input_type: Hash, build: :build_oauth_flow
-      field "clientCredentials", input_type: Hash, build: :build_oauth_flow
-      field "authorizationCode", input_type: Hash, build: :build_oauth_flow
+      include Node::Object
 
       def implicit
-        fields["implicit"]
+        node_data["implicit"]
       end
 
       def password
-        fields["password"]
+        node_data["password"]
       end
 
       def client_credentials
-        fields["clientCredentials"]
+        node_data["clientCredentials"]
       end
 
       def authorization_code
-        fields["authorizationCode"]
-      end
-
-      private
-
-      def build_oauth_flow(input, context)
-        OpenAuthFlow.new(input, context)
+        node_data["authorizationCode"]
       end
     end
   end
