52a5bbb665e8cd0f192f03a6debfe649abd42574
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-20 21:45:53 +0000

    Header node

--- lib/openapi_parser/nodes/header.rb
+++ lib/openapi_parser/nodes/header.rb
@@ -1,38 +1,13 @@
 # frozen_string_literal: true
 
-require "openapi_parser/node"
-require "openapi_parser/nodes/external_documentation"
+require "openapi_parser/node/object"
+require "openapi_parser/nodes/parameter/parameter_like"
 
 module OpenapiParser
   module Nodes
     class Header
-      include Node
-
-      allow_extensions
-
-      field "name", input_type: String, required: true
-      field "description", input_type: String
-      field "externalDocs",
-            input_type: Hash,
-            build: :build_external_docs
-
-      def name
-        fields["name"]
-      end
-
-      def description
-        fields["description"]
-      end
-
-      def external_docs
-        fields["externalDocs"]
-      end
-
-      private
-
-      def build_external_docs(input, context)
-        ExternalDocumentation.new(input, context)
-      end
+      include Node::Object
+      include Parameter::ParameterLike
     end
   end
 end
