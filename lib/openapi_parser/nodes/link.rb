a21a7cf1f7562e67f300212f0ed8277623ee0076
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-22 20:37:18 +0000

    Add link node

--- lib/openapi_parser/nodes/link.rb
+++ lib/openapi_parser/nodes/link.rb
@@ -1,51 +1,49 @@
 # frozen_string_literal: true
 
-require "openapi_parser/node"
-require "openapi_parser/nodes/server"
-require "openapi_parser/fields/map"
+require "openapi_parser/node/object"
 
 module OpenapiParser
   module Nodes
     class Link
-      include Node
+      include Node::Object
 
-      allow_extensions
-
-      # @TODO The link object in OAS is pretty meaty and there's lot of scope
-      # for further work here to make use of it's funcationality
-      field "operationRef", input_type: String
-      field "operationId", input_type: String
-      field "parameters",
-            input_type: Hash,
-            build: ->(input, context) { Fields::Map.call(input, context) }
-      field "requestBody"
-      field "description", input_type: String
-      field "server",
-            input_type: Hash,
-            build: ->(input, context) { Server.new(input, context) }
-
+      # allow_extensions
+      #
+      # # @TODO The link object in OAS is pretty meaty and there's lot of scope
+      # # for further work here to make use of it's funcationality
+      # field "operationRef", input_type: String
+      # field "operationId", input_type: String
+      # field "parameters",
+      #       input_type: Hash,
+      #       build: ->(input, context) { Fields::Map.call(input, context) }
+      # field "requestBody"
+      # field "description", input_type: String
+      # field "server",
+      #       input_type: Hash,
+      #       build: ->(input, context) { Server.new(input, context) }
+      #
       def operation_ref
-        fields["operationRef"]
+        node_data["operationRef"]
       end
 
       def operation_id
-        fields["operationId"]
+        node_data["operationId"]
       end
 
       def parameters
-        fields["parameters"]
+        node_data["parameters"]
       end
 
       def request_body
-        fields["requestBody"]
+        node_data["requestBody"]
       end
 
       def description
-        fields["description"]
+        node_data["description"]
       end
 
       def server
-        fields["server"]
+        node_data["server"]
       end
     end
   end
