6a185ec863783d58d17cd6c4e38f1a678f700407
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-27 21:27:14 +0000

    Document return types for OpenAPI Node

--- lib/openapi3_parser/nodes/openapi.rb
+++ lib/openapi3_parser/nodes/openapi.rb
@@ -8,34 +8,43 @@ module Openapi3Parser
     class Openapi
       include Node::Object
 
+      # @return [String]
       def openapi
         node_data["openapi"]
       end
 
+      # @return [Info]
       def info
         node_data["info"]
       end
 
+      # @return [Nodes::Array] A collection of {Server}[../Server.html] objects
       def servers
         node_data["servers"]
       end
 
+      # @return [Paths]
       def paths
         node_data["paths"]
       end
 
+      # @return [Components, nil]
       def components
         node_data["components"]
       end
 
+      # @return [Nodes::Array] a collection of
+      #         {SecurityRequirement}[../SecurityRequirement.html] objects
       def security
         node_data["security"]
       end
 
+      # @return [Nodes::Array] A collection of {Tag}[../Tag.html] objects
       def tags
         node_data["tags"]
       end
 
+      # @return [ExternalDocumentation]
       def external_docs
         node_data["externalDocs"]
       end
