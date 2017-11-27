1d4ce70cf8d95b526e1841e34e209b0c5334346e
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-27 21:04:20 +0000

    Documentation for root methods

--- lib/openapi3_parser.rb
+++ lib/openapi3_parser.rb
@@ -7,6 +7,12 @@ require "yaml"
 require "json"
 
 module Openapi3Parser
+  # For a variety of inputs this will construct an OpenAPI document. For a
+  # String/File input it will try to determine if the input is JSON or YAML.
+  #
+  # @param [String, Hash, File] input Source for the OpenAPI document
+  #
+  # @return [Document]
   def self.load(input)
     # working_directory ||= if input.respond_to?(:read)
     #                         File.dirname(input)
@@ -17,6 +23,13 @@ module Openapi3Parser
     Document.new(parse_input(input))
   end
 
+  # For a given string filename this will read the file and parse it as an
+  # OpenAPI document. It will try detect automatically whether the contents
+  # are JSON or YAML.
+  #
+  # @param [String] path Filename of the OpenAPI document
+  #
+  # @return [Document]
   def self.load_file(path)
     file = File.open(path)
     load(file)
