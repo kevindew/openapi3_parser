8f85dbc678b1233a40845fd69742e713a5bcccbb
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 22:01:49 +0000

    Documentation for ExternalDocumentation node

--- lib/openapi3_parser/nodes/external_documentation.rb
+++ lib/openapi3_parser/nodes/external_documentation.rb
@@ -4,13 +4,16 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#externalDocumentationObject
     class ExternalDocumentation
       include Node::Object
 
+      # @return [String, nil]
       def description
         node_data["description"]
       end
 
+      # @return [String]
       def url
         node_data["url"]
       end
