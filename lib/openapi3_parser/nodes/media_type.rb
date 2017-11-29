0b6002f1f8227df4890241b00b2fb74c404db2d5
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:46:38 +0000

    Documentation for MediaType node

--- lib/openapi3_parser/nodes/media_type.rb
+++ lib/openapi3_parser/nodes/media_type.rb
@@ -4,21 +4,26 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#mediaTypeObject
     class MediaType
       include Node::Object
 
+      # @return [Schema, nil]
       def schema
         node_data["schema"]
       end
 
+      # @return [Any]
       def example
         node_data["example"]
       end
 
+      # @return [Map] a map of String: {Example}[./Example.html] objects
       def examples
         node_data["examples"]
       end
 
+      # @return [Map] a map of String: {Encoding}[./Encoding.html] objects
       def encoding
         node_data["encoding"]
       end
