cf76dcb49029a41f10a83922fb6d0fa84dfc29b0
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:31:21 +0000

    Documentation for Info node

--- lib/openapi3_parser/nodes/info.rb
+++ lib/openapi3_parser/nodes/info.rb
@@ -4,29 +4,36 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#infoObject
     class Info
       include Node::Object
 
+      # @return [String]
       def title
         node_data["title"]
       end
 
+      # @return [String, null]
       def description
         node_data["description"]
       end
 
+      # @return [String, null]
       def terms_of_service
         node_data["termsOfService"]
       end
 
+      # @return [Contact, nil]
       def contact
         node_data["contact"]
       end
 
+      # @return [License, nil]
       def license
         node_data["license"]
       end
 
+      # @return [String]
       def version
         node_data["version"]
       end
