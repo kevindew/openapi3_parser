58874b555c5995d0e10f09c32e7664edfbbc0669
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 22:00:10 +0000

    Documentation for Encoding node

--- lib/openapi3_parser/nodes/encoding.rb
+++ lib/openapi3_parser/nodes/encoding.rb
@@ -4,25 +4,31 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#encodingObject
     class Encoding
       include Node::Object
 
+      # @return [String, nil]
       def content_type
         node_data["contentType"]
       end
 
+      # @return [Map] a map of String: {Header}[./Header.html] objects
       def headers
         node_data["headers"]
       end
 
+      # @return [String, nil]
       def style
         node_data["style"]
       end
 
+      # @return [Boolean]
       def explode?
         node_data["explode"]
       end
 
+      # @return [Boolean]
       def allow_reserved?
         node_data["allowReserved"]
       end
