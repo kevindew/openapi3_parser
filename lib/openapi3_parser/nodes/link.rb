e6e22e70b605cf2ed4ed4bbc302409b3f9992ba2
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:40:24 +0000

    Documentation for Link node

--- lib/openapi3_parser/nodes/link.rb
+++ lib/openapi3_parser/nodes/link.rb
@@ -4,29 +4,36 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#linkObject
     class Link
       include Node::Object
 
+      # @return [String, nil]
       def operation_ref
         node_data["operationRef"]
       end
 
+      # @return [String, nil]
       def operation_id
         node_data["operationId"]
       end
 
+      # @return [Map] a map of String: {Parameter}[./Parameter.html] objects
       def parameters
         node_data["parameters"]
       end
 
+      # @return [Any]
       def request_body
         node_data["requestBody"]
       end
 
+      # @return [String, nil]
       def description
         node_data["description"]
       end
 
+      # @return [Server, nil]
       def server
         node_data["server"]
       end
