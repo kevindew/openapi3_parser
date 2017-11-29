90a4676bbd5385fe29e162fdad8c54487e711ee6
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:48:48 +0000

    Documentation for OauthFlow node

--- lib/openapi3_parser/nodes/oauth_flow.rb
+++ lib/openapi3_parser/nodes/oauth_flow.rb
@@ -4,21 +4,26 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#oauthFlowObject
     class OauthFlow
       include Node::Object
 
+      # @return [String, nil]
       def authorization_url
         node_data["authorizationUrl"]
       end
 
+      # @return [String, nil]
       def token_url
         node_data["tokenUrl"]
       end
 
+      # @return [String, nil]
       def refresh_url
         node_data["refreshUrl"]
       end
 
+      # @return [Map] a map of String: String objects
       def scopes
         node_data["scopes"]
       end
