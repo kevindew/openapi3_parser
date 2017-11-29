33bf1ac2e3a1d305a0d63cf85ef68209e766e365
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:56:32 +0000

    Documentation for Parameter node

--- lib/openapi3_parser/nodes/parameter.rb
+++ lib/openapi3_parser/nodes/parameter.rb
@@ -5,14 +5,17 @@ require "openapi3_parser/nodes/parameter/parameter_like"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#parameterObject
     class Parameter
       include Node::Object
       include ParameterLike
 
+      # @return [String]
       def name
         node_data["name"]
       end
 
+      # @return [String]
       def in
         node_data["in"]
       end
