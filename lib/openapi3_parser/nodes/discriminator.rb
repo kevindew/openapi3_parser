e275993b04c8d26538a96124e9ae8179dc47be1a
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 21:51:43 +0000

    Documentation for Discriminator node

--- lib/openapi3_parser/nodes/discriminator.rb
+++ lib/openapi3_parser/nodes/discriminator.rb
@@ -4,13 +4,16 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#discriminatorObject
     class Discriminator
       include Node::Object
 
+      # @return [String]
       def property_name
         node_data["propertyName"]
       end
 
+      # @return [Map] a map of String: String objects
       def mapping
         node_data["mapping"]
       end
