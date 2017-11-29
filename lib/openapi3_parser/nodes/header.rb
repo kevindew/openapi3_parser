8c748daf565da35040faab6d704ac79ed55e6912
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:41:06 +0000

    Documentation for Header node

--- lib/openapi3_parser/nodes/header.rb
+++ lib/openapi3_parser/nodes/header.rb
@@ -5,6 +5,7 @@ require "openapi3_parser/nodes/parameter/parameter_like"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#headerObject
     class Header
       include Node::Object
       include Parameter::ParameterLike
