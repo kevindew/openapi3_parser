2418de854a1f62457a8f8791f86ff18ae43719f2
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 23:50:08 +0000

    Documentation for OauthFlows node

--- lib/openapi3_parser/nodes/oauth_flows.rb
+++ lib/openapi3_parser/nodes/oauth_flows.rb
@@ -4,21 +4,26 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#oauthFlowsObject
     class OauthFlows
       include Node::Object
 
+      # @return [OauthFlow, nil]
       def implicit
         node_data["implicit"]
       end
 
+      # @return [OauthFlow, nil]
       def password
         node_data["password"]
       end
 
+      # @return [OauthFlow, nil]
       def client_credentials
         node_data["clientCredentials"]
       end
 
+      # @return [OauthFlow, nil]
       def authorization_code
         node_data["authorizationCode"]
       end
