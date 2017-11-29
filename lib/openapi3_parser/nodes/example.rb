432a32b4e5d3a5bd73c81517765f5e2152ffc3c6
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-29 22:00:24 +0000

    Documentation for Example node

--- lib/openapi3_parser/nodes/example.rb
+++ lib/openapi3_parser/nodes/example.rb
@@ -4,21 +4,26 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#exampleObject
     class Example
       include Node::Object
 
+      # @return [String, nil]
       def summary
         node_data["summary"]
       end
 
+      # @return [String, nil]
       def description
         node_data["description"]
       end
 
+      # @return [Object]
       def value
         node_data["value"]
       end
 
+      # @return [String, nil]
       def external_value
         node_data["externalValue"]
       end
