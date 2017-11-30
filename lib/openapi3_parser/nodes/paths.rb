d0a364010d4f5d18868dc64ffee5f9932e3c2e7d
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-30 20:30:44 +0000

    Documentation for Paths node

--- lib/openapi3_parser/nodes/paths.rb
+++ lib/openapi3_parser/nodes/paths.rb
@@ -4,6 +4,7 @@ require "openapi3_parser/node/map"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#pathsObject
     class Paths
       include Node::Map
     end
