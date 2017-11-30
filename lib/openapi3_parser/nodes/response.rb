8b5330a853f26ef0d1654d21d78223be3a67d760
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-30 20:33:23 +0000

    Documentation for Response node

--- lib/openapi3_parser/nodes/response.rb
+++ lib/openapi3_parser/nodes/response.rb
@@ -4,21 +4,26 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#responseObject
     class Response
       include Node::Object
 
+      # @return [String]
       def description
         node_data["description"]
       end
 
+      # @return [Map] a map of String: {Header}[./Header.html] objects
       def headers
         node_data["headers"]
       end
 
+      # @return [Map] a map of String: {MediaType}[./MediaType.html] objects
       def content
         node_data["content"]
       end
 
+      # @return [Map] a map of String: {Link}[./Link.html] objects
       def links
         node_data["links"]
       end
