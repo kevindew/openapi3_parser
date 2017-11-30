530f09e0b44b7a19b10f7b3877027774f089d2a6
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-30 20:49:58 +0000

    Documentation for Responses node

--- lib/openapi3_parser/nodes/responses.rb
+++ lib/openapi3_parser/nodes/responses.rb
@@ -4,9 +4,11 @@ require "openapi3_parser/node/map"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#responsesObject
     class Responses
       include Node::Map
 
+      # @return [Response]
       def default
         node_data["default"]
       end
