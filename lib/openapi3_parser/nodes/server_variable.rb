c2ae473663935dda8a1385c8677ce8e324813093
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-11-30 21:42:58 +0000

    Documentation for ServerVariable node

--- lib/openapi3_parser/nodes/server_variable.rb
+++ lib/openapi3_parser/nodes/server_variable.rb
@@ -4,17 +4,21 @@ require "openapi3_parser/node/object"
 
 module Openapi3Parser
   module Nodes
+    # @see https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#serverVariableObject
     class ServerVariable
       include Node::Object
 
+      # @return [Nodes::Array, nil] a collection of String objects or nil
       def enum
         node_data["enum"]
       end
 
+      # @return [String]
       def default
         node_data["default"]
       end
 
+      # @return [String, nil]
       def description
         node_data["description"]
       end
