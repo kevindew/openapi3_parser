3c9b833e06a9f5f91c68e698f419ad213ae806aa
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-12-20 23:12:32 +0000

    Integration test for an invalid document

---
+++ spec/integration/open_an_invalid_document_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "openapi3_parser"
+require "openapi3_parser/error"
+
+RSpec.describe "Open an invalid document" do
+  subject(:document) { Openapi3Parser.load(input) }
+
+  let(:input) do
+    {
+      openapi: "3.0.0",
+      info: {
+        title: "Test Document",
+        version: "1.0.0"
+      },
+      paths: {},
+      components: {
+        examples: {
+          test: { extra: "field" }
+        }
+      }
+    }
+  end
+
+  it { is_expected.to_not be_valid }
+
+  it "raises an exception accessing the root" do
+    expect { document.root }.to raise_error(Openapi3Parser::Error)
+  end
+end
