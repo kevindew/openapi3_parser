776c70d6349ad8db2c659fcffdcbfca69fb0158f
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-12-20 00:01:20 +0000

    Start creating specific errors
    Eventually all the errors should have a specific class and we'll move
    away from these being wrapped in a generic error class.

--- lib/openapi3_parser/error.rb
+++ lib/openapi3_parser/error.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 module Openapi3Parser
-  class Error < ::RuntimeError; end
+  class Error < ::RuntimeError
+    class InaccessibleInput < Error; end
+    class UnparsableInput < Error; end
+  end
 end
