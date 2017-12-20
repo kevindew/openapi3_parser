53e9f86c91f88d7a1151ece8587f31d565d07a99
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-12-20 00:09:19 +0000

    Validator for a reference
    To be used when resolving references.

---
+++ lib/openapi3_parser/validators/reference.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Openapi3Parser
+  module Validators
+    class Reference
+      def initialize(given_reference)
+        @given_reference = given_reference
+      end
+
+      def valid?
+        errors.empty?
+      end
+
+      def errors
+        @errors ||= Array(build_errors)
+      end
+
+      private
+
+      attr_reader :given_reference
+
+      def build_errors
+        return "expected a string" unless given_reference.is_a?(String)
+        begin
+          uri = URI.parse(given_reference)
+        rescue URI::Error
+          return "could not parse as a URI"
+        end
+        check_fragment(uri) || []
+      end
+
+      def check_fragment(uri)
+        return if uri.fragment.nil? || uri.fragment.empty?
+        first_char = uri.fragment[0]
+
+        "invalid JSON pointer, expected a root slash" if first_char != "/"
+      end
+    end
+  end
+end
---
+++ spec/lib/openapi3_parser/validators/reference_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "openapi3_parser/validators/reference"
+
+RSpec.describe Openapi3Parser::Validators::Reference do
+  describe ".errors" do
+    let(:input) { "#/test" }
+    subject { described_class.new(input).errors }
+
+    it { is_expected.to be_an_instance_of(Array) }
+
+    context "when it's valid" do
+      it { is_expected.to be_empty }
+    end
+
+    context "when it's not a string" do
+      let(:input) { 12 }
+      let(:errors) { ["expected a string"] }
+      it { is_expected.to match_array(errors) }
+    end
+
+    context "when it's an invalid URI" do
+      let(:input) { "test test test" }
+      let(:errors) { ["could not parse as a URI"] }
+      it { is_expected.to match_array(errors) }
+    end
+
+    context "when it's an invalid JSON pointer" do
+      let(:input) { "./test#any-old-fragment" }
+      let(:errors) { ["invalid JSON pointer, expected a root slash"] }
+      it { is_expected.to match_array(errors) }
+    end
+  end
+
+  describe ".valid?" do
+    let(:input) { "#/test" }
+    subject { described_class.new(input).valid? }
+
+    context "when it's valid" do
+      it { is_expected.to be true }
+    end
+
+    context "when it's invalid" do
+      let(:input) { "test test" }
+      it { is_expected.to be false }
+    end
+  end
+end
