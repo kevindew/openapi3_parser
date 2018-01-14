2bf7a5fd420fef0e91776b8a5867f335205f86f4
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-01-14 22:02:16 +0000

    Update the root file
    It was missing documentation and had unnecessary requires

--- lib/openapi3_parser.rb
+++ lib/openapi3_parser.rb
@@ -1,28 +1,18 @@
 # frozen_string_literal: true
 
-require "openapi3_parser/error"
 require "openapi3_parser/document"
 require "openapi3_parser/source_input/raw"
 require "openapi3_parser/source_input/file"
 require "openapi3_parser/source_input/url"
 
-require "yaml"
-require "json"
-
 module Openapi3Parser
   # For a variety of inputs this will construct an OpenAPI document. For a
   # String/File input it will try to determine if the input is JSON or YAML.
   #
-  # @param [String, Hash, File] input Source for the OpenAPI document
+  # @param  [String, Hash, File]  input Source for the OpenAPI document
   #
   # @return [Document]
   def self.load(input)
-    # working_directory ||= if input.respond_to?(:read)
-    #                         File.dirname(input)
-    #                       else
-    #                         Dir.pwd
-    #                       end
-
     Document.new(SourceInput::Raw.new(input))
   end
 
@@ -30,13 +20,20 @@ module Openapi3Parser
   # OpenAPI document. It will try detect automatically whether the contents
   # are JSON or YAML.
   #
-  # @param [String] path Filename of the OpenAPI document
+  # @param  [String]  path  Filename of the OpenAPI document
   #
   # @return [Document]
   def self.load_file(path)
     Document.new(SourceInput::File.new(path))
   end
 
+  # For a given string URL this will request the resource and parse it as an
+  # OpenAPI document. It will try detect automatically whether the contents
+  # are JSON or YAML.
+  #
+  # @param  [String]  url URL of the OpenAPI document
+  #
+  # @return [Document]
   def self.load_url(url)
     Document.new(SourceInput::Url.new(url.to_s))
   end
