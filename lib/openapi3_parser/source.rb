04816fdefdf13d878e3dba132afa9278372acfd3
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2018-01-14 23:08:10 +0000

    Update documentation on Source

--- lib/openapi3_parser/source.rb
+++ lib/openapi3_parser/source.rb
@@ -5,9 +5,26 @@ require "openapi3_parser/source/reference"
 require "openapi3_parser/source/reference_resolver"
 
 module Openapi3Parser
+  # Represents a source of data used to produce the OpenAPI document. Documents
+  # which do not have any references to external files will only have a single
+  # source
+  #
+  # @attr_reader [SourceInput]                  source_input
+  #   The source input which provides the data
+  # @attr_reader [Document]                     document
+  #   The document that this source is associated with
+  # @attr_reader [Document::ReferenceRegister]  reference_register
+  #   An object to track references for a document
+  # @attr_reader [Source, nil]                  parent
+  #   Set to a Source if this source was created due to a reference within
+  #   a different Source
   class Source
     attr_reader :source_input, :document, :reference_register, :parent
 
+    # @param  [SourceInput]                   source_input
+    # @param  [Document]                      document
+    # @param  [Document::ReferenceRegister]   reference_register
+    # @param  [Source, nil]                   parent
     def initialize(source_input, document, reference_register, parent = nil)
       @source_input = source_input
       @document = document
@@ -15,14 +32,30 @@ module Openapi3Parser
       @parent = parent
     end
 
+    # The data from the source
     def data
       @data ||= normalize_data(source_input.contents)
     end
 
+    # @see SourceInput#available?
     def available?
       source_input.available?
     end
 
+    # Whether this is the root source of a document
+    def root?
+      document.root_source == self
+    end
+
+    # Used to register a reference with the underlying document and return a
+    # reference resolver to access the object referenced
+    #
+    # @param  [String]        given_reference   The reference as text
+    # @param  [NodeFactory]   factory           Factory class for the expected
+    #                                           eventual resource
+    # @param  [Context]       context           The context of the object
+    #                                           calling this reference
+    # @return [ReferenceResolver]
     def register_reference(given_reference, factory, context)
       reference = Reference.new(given_reference)
       ReferenceResolver.new(
@@ -33,6 +66,10 @@ module Openapi3Parser
       end
     end
 
+    # Access/create the source object for a reference
+    #
+    # @param  [Reference] reference
+    # @return [Source]
     def resolve_source(reference)
       if reference.only_fragment?
         # I found the spec wasn't fully clear on expected behaviour if a source
@@ -40,8 +77,6 @@ module Openapi3Parser
         # and just the root source. I'm assuming to be consistent with URI a
         # fragment only reference only references current JSON document. This
         # could be incorrect though.
-        #
-        # @todo confirm this behaviour
         self
       else
         next_source_input = source_input.resolve_next(reference)
@@ -53,11 +88,16 @@ module Openapi3Parser
       end
     end
 
+    # Access the data in a source at a particular pointer
+    #
+    # @param  [Array] json_pointer  An array of segments of a JSON pointer
+    # @return [Object]
     def data_at_pointer(json_pointer)
       return data if json_pointer.empty?
       data.dig(*json_pointer) if data.respond_to?(:dig)
     end
 
+    # Whether the source has data at the particular pointer
     def has_pointer?(json_pointer) # rubocop:disable Naming/PredicateName
       !data_at_pointer(json_pointer).nil?
     end
