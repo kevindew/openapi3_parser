422d8934924f90ce68debf03c9fa08637c279b20
Kevin Dew <kevindew@me.com>
2018-03-19 12:19:09 +0000

    Validate Terms of Service URL on Info

--- spec/lib/openapi3_parser/node_factories/info_spec.rb
+++ spec/lib/openapi3_parser/node_factories/info_spec.rb
@@ -8,17 +8,39 @@ require "support/helpers/context"
 
 RSpec.describe Openapi3Parser::NodeFactories::Info do
   include Helpers::Context
+  let(:minimal_info_definition) do
+    {
+      "title" => "Info",
+      "version" => "1.0"
+    }
+  end
 
   it_behaves_like "node object factory", Openapi3Parser::Node::Info do
     let(:input) do
-      {
-        "title" => "Info",
+      minimal_info_definition.merge(
         "license" => { "name" => "License" },
-        "contact" => { "name" => "Contact" },
-        "version" => "1.0"
-      }
+        "contact" => { "name" => "Contact" }
+      )
     end
 
     let(:context) { create_context(input) }
+  end
+
+  describe "terms of service" do
+    subject(:factory) { described_class.new(context) }
+    let(:input) do
+      minimal_info_definition.merge("termsOfService" => terms_of_service)
+    end
+    let(:context) { create_context(input) }
+
+    context "when terms of service is a url" do
+      let(:terms_of_service) { "https://example.com/path" }
+      it { is_expected.to be_valid }
+    end
+
+    context "when terms of service is not a url" do
+      let(:terms_of_service) { "not a url" }
+      it { is_expected.not_to be_valid }
+    end
   end
 end
