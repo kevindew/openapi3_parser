89d72bdbaf96a09658647acb719d6b037a30569e
Kevin Dew <kevindew@me.com>
2018-04-01 22:55:50 +0100

    Validate links key formats for response
    This validates that the same format that is used for component keys is
    used to validate the keys of links within a Response node.

--- lib/openapi3_parser/node_factories/response.rb
+++ lib/openapi3_parser/node_factories/response.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "openapi3_parser/context"
 require "openapi3_parser/node/response"
 require "openapi3_parser/node_factory/object"
 require "openapi3_parser/node_factory/optional_reference"
@@ -7,6 +8,9 @@ require "openapi3_parser/node_factories/map"
 require "openapi3_parser/node_factories/header"
 require "openapi3_parser/node_factories/media_type"
 require "openapi3_parser/node_factories/link"
+require "openapi3_parser/validation/error"
+require "openapi3_parser/validators/media_type"
+require "openapi3_parser/validators/component_keys"
 
 module Openapi3Parser
   module NodeFactories
@@ -32,13 +36,29 @@ module Openapi3Parser
 
       def content_factory(context)
         NodeFactories::Map.new(
-          context, value_factory: NodeFactories::MediaType
+          context,
+          validate: method(:validate_content),
+          value_factory: NodeFactories::MediaType
         )
       end
 
       def links_factory(context)
         factory = NodeFactory::OptionalReference.new(NodeFactories::Link)
-        NodeFactories::Map.new(context, value_factory: factory)
+        NodeFactories::Map.new(
+          context,
+          validate: ->(input, _) { Validators::ComponentKeys.call(input) },
+          value_factory: factory
+        )
+      end
+
+      def validate_content(input, context)
+        input.keys.each_with_object([]) do |key, memo|
+          message = Validators::MediaType.call(key)
+          next unless message
+          memo << Validation::Error.new(
+            message, Context.next_field(context, key)
+          )
+        end
       end
     end
   end
--- spec/lib/openapi3_parser/node_factories/response_spec.rb
+++ spec/lib/openapi3_parser/node_factories/response_spec.rb
@@ -42,4 +42,61 @@ RSpec.describe Openapi3Parser::NodeFactories::Response do
 
     let(:context) { create_context(input) }
   end
+
+  describe "content" do
+    subject { described_class.new(context) }
+    let(:context) do
+      create_context("description" => "Description", "content" => content)
+    end
+
+    context "when content is an empty hash" do
+      let(:content) { {} }
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when content has a valid media type" do
+      let(:content) do
+        {
+          "application/json" => {}
+        }
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when content has an invalid valid media type" do
+      let(:content) do
+        {
+          "bad-media-type" => {}
+        }
+      end
+
+      it do
+        is_expected
+          .to have_validation_error("#/content/bad-media-type")
+          .with_message(%("bad-media-type" is not a valid media type))
+      end
+    end
+  end
+
+  describe "links" do
+    subject { described_class.new(context) }
+    let(:context) do
+      create_context(
+        "description" => "Description",
+        "links" => { key => {} }
+      )
+    end
+
+    context "when key is invalid" do
+      let(:key) { "Invalid Key" }
+      it { is_expected.not_to be_valid }
+    end
+
+    context "when key is valid" do
+      let(:key) { "valid.key" }
+      it { is_expected.to be_valid }
+    end
+  end
 end
