ca44d3d20c0eec0d348a38db9a4a4b40a86d1415
Kevin Dew <kevindew@me.com>
2018-04-01 22:35:54 +0100

    Dedicated component keys validator
    This was created as a separate class since there is a need to validate
    component keys outside of just the Components node (it's used for links
    in a Response too).

---
+++ lib/openapi3_parser/validators/component_keys.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Openapi3Parser
+  module Validators
+    # This validates that the keys of an object match the format of those
+    # defined for a Components node.
+    # As defined: https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#components-object
+    class ComponentKeys
+      REGEX = /\A[a-zA-Z0-9\.\-_]+\Z/
+
+      def self.call(input)
+        invalid = input.keys.reject { |key| REGEX.match(key) }
+        "Contains invalid keys: #{invalid.join(', ')}" unless invalid.empty?
+      end
+    end
+  end
+end
---
+++ spec/lib/openapi3_parser/validators/component_keys_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "openapi3_parser/validators/component_keys"
+
+RSpec.describe Openapi3Parser::Validators::ComponentKeys do
+  describe ".call" do
+    subject { described_class.call(key => {}) }
+
+    context "when input is an invalid component key" do
+      let(:key) { "Invalid Key" }
+      it { is_expected.to eq "Contains invalid keys: Invalid Key" }
+    end
+
+    context "when input is a valid component key" do
+      let(:key) { "valid.key" }
+      it { is_expected.to be_nil }
+    end
+  end
+end
