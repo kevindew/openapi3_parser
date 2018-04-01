8521c9dafe5d152554a5548f7030a708eb31cd8b
Kevin Dew <kevindew@me.com>
2018-04-01 17:15:55 +0100

    Media Type validator
    This is due to be used with the request_body type.

---
+++ lib/openapi3_parser/validators/media_type.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Openapi3Parser
+  module Validators
+    class MediaType
+      REGEX = %r{
+        \A
+        (\w+|\*) # word or asterisk
+        / # separating slash
+        ([-+.\w]+|\*) # word (with +, - & .) or asterisk
+        \Z
+      }x
+
+      def self.call(input)
+        message = %("#{input}" is not a valid media type)
+        message unless REGEX.match(input)
+      end
+    end
+  end
+end
---
+++ spec/lib/openapi3_parser/validators/media_type_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require "openapi3_parser/validators/media_type"
+
+RSpec.describe Openapi3Parser::Validators::MediaType do
+  describe ".call" do
+    subject { described_class.call(media_type) }
+
+    context "when input is not a media type" do
+      let(:media_type) { "not an media type" }
+      it { is_expected.to eq %("#{media_type}" is not a valid media type) }
+    end
+
+    context "when media type is valid" do
+      let(:valid_media_types) do
+        %w[
+          */*
+          text/*
+          text/plain
+          application/atom+xml
+          application/EDI-X12
+          application/xml-dtd
+          application/zip
+          application/vnd.openxmlformats-officedocument.presentationml
+          video/quicktime
+        ]
+      end
+
+      it "doesn't return an error on any of them" do
+        valid_media_types.each do |media_type|
+          expect(described_class.call(media_type)).to be_nil
+        end
+      end
+    end
+  end
+end
