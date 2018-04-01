50217343c74bdd857ed73ba4cf871f527dab8668
Kevin Dew <kevindew@me.com>
2018-04-01 17:16:40 +0100

    Validate content on Request Body

--- lib/openapi3_parser/node_factories/request_body.rb
+++ lib/openapi3_parser/node_factories/request_body.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require "openapi3_parser/context"
 require "openapi3_parser/node/request_body"
 require "openapi3_parser/node_factory/object"
 require "openapi3_parser/node_factories/media_type"
 require "openapi3_parser/node_factories/map"
+require "openapi3_parser/validation/error"
+require "openapi3_parser/validators/media_type"
 
 module Openapi3Parser
   module NodeFactories
@@ -23,8 +26,34 @@ module Openapi3Parser
 
       def content_factory(context)
         NodeFactories::Map.new(
-          context, value_factory: NodeFactories::MediaType
+          context,
+          value_factory: NodeFactories::MediaType,
+          validate: ContentValidator
         )
+      end
+
+      class ContentValidator
+        def self.call(*args)
+          new.call(*args)
+        end
+
+        def call(input, context)
+          # This validation isn't actually mentioned in the spec, but it
+          # doesn't seem to make sense if this is an empty hash.
+          return "Expected to have at least 1 item" if input.size.zero?
+
+          input.keys.each_with_object([]) do |key, memo|
+            message = Validators::MediaType.call(key)
+            memo << create_error(key, context, message) if message
+          end
+        end
+
+        private
+
+        def create_error(key, parent_context, message)
+          context = Context.next_field(parent_context, key)
+          Validation::Error.new(message, context)
+        end
       end
     end
   end
--- spec/lib/openapi3_parser/node_factories/request_body_spec.rb
+++ spec/lib/openapi3_parser/node_factories/request_body_spec.rb
@@ -26,4 +26,53 @@ RSpec.describe Openapi3Parser::NodeFactories::RequestBody do
 
     let(:context) { create_context(input) }
   end
+
+  describe "content" do
+    subject { described_class.new(context) }
+    let(:context) { create_context("content" => content) }
+
+    context "when content is an empty hash" do
+      let(:content) { {} }
+
+      it do
+        is_expected
+          .to have_validation_error("#/content")
+          .with_message("Expected to have at least 1 item")
+      end
+    end
+
+    context "when content has a valid media type" do
+      let(:content) do
+        {
+          "application/json" => {}
+        }
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when content has a valid media type range" do
+      let(:content) do
+        {
+          "text/*" => {}
+        }
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "when content has an invalid valid media type" do
+      let(:content) do
+        {
+          "bad-media-type" => {}
+        }
+      end
+
+      it do
+        is_expected
+          .to have_validation_error("#/content/bad-media-type")
+          .with_message(%("bad-media-type" is not a valid media type))
+      end
+    end
+  end
 end
