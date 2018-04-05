853a3570dbfe4703f77553fc11290ee96dc0ca51
Kevin Dew <kevindew@me.com>
2018-04-05 22:25:02 +0100

    Tests for default fields on Schema
    Creates a shared helper for default values

--- spec/lib/openapi3_parser/node_factories/schema_spec.rb
+++ spec/lib/openapi3_parser/node_factories/schema_spec.rb
@@ -3,6 +3,7 @@
 require "openapi3_parser/node_factories/schema"
 require "openapi3_parser/node/schema"
 
+require "support/default_field"
 require "support/node_object_factory"
 require "support/helpers/context"
 
@@ -73,4 +74,25 @@ RSpec.describe Openapi3Parser::NodeFactories::Schema do
       it { is_expected.to be_valid }
     end
   end
+
+  it_behaves_like "default field", field: "nullable", defaults_to: false do
+    let(:context) { create_context("nullable" => nullable) }
+  end
+
+  it_behaves_like "default field",
+                  field: "readOnly", defaults_to: false, var_name: :read_only do
+                    let(:context) { create_context("readOnly" => read_only) }
+                  end
+
+  it_behaves_like "default field",
+                  field: "writeOnly", defaults_to: false,
+                  var_name: :write_only do
+                    let(:context) { create_context("writeOnly" => write_only) }
+                  end
+
+  it_behaves_like "default field",
+                  field: "deprecated", defaults_to: false,
+                  var_name: :deprecated do
+                    let(:context) { create_context("deprecated" => deprecated) }
+                  end
 end
---
+++ spec/support/default_field.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples "default field" do |field:, defaults_to:, var_name: nil|
+  var_name ||= field.to_sym
+
+  subject(:node) { described_class.new(context).node }
+
+  context "when #{field} is not set" do
+    let(var_name) { nil }
+    it "has a default value of #{defaults_to}" do
+      expect(node[field]).to eq defaults_to
+    end
+  end
+end
