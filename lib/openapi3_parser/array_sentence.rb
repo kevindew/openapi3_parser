ed211efe77981f600f0256812edaecece81cb5db
Kevin Dew <kevindew@me.com>
2018-04-29 00:24:12 +0100

    Array refinement for sentence joining
    This allows an array of strings to be joined in the form of a sentence.

---
+++ lib/openapi3_parser/array_sentence.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Openapi3Parser
+  module ArraySentence
+    refine ::Array do
+      def sentence_join
+        return join if count < 2
+        self[0..-2].join(", ") + " and " + self[-1]
+      end
+    end
+  end
+end
