aa20fb1c39df3014c8b0d1388109431180c9807b
Kevin Dew <kevindew@me.com>
2018-05-30 19:11:03 +0100

    Validatable class
    This class is an attempt to solve the complexities involed in what to
    pass around when validating nodes. Rather than passing around input or
    context arguments this can be passed around which has access to those
    fields and errors can be added directly to it rather than worrying about
    returning strings or arrays.

---
+++ lib/openapi3_parser/validation/validatable.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "openapi3_parser/validation/error"
+require "openapi3_parser/validation/error_collection"
+
+module Openapi3Parser
+  module Validation
+    class Validatable
+      attr_reader :context, :errors, :factory
+
+      UNDEFINED = Class.new
+
+      def initialize(factory, context: nil)
+        @factory = factory
+        @context = context || factory.context
+        @errors = []
+      end
+
+      def input
+        context.input
+      end
+
+      def add_error(error, given_context = nil, factory_class = UNDEFINED)
+        return unless error
+        return @errors << error if error.is_a?(Validation::Error)
+
+        @errors << Validation::Error.new(
+          error,
+          given_context || context,
+          factory_class == UNDEFINED ? factory.class : factory_class
+        )
+      end
+
+      def add_errors(errors)
+        errors = errors.to_a if errors.respond_to?(:to_a)
+        errors.each { |e| add_error(e) }
+      end
+
+      def collection
+        ErrorCollection.new(errors)
+      end
+    end
+  end
+end
