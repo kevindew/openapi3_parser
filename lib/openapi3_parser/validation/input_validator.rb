0e52b046f2e5b73781c9df97e3368b3164247d43
Kevin Dew <kevindew@me.com>
2018-05-30 19:14:35 +0100

    Validation wrapper
    This class acts as a bridge between validators that expect input and
    return a string and instances of validatable.

---
+++ lib/openapi3_parser/validation/input_validator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Openapi3Parser
+  module Validation
+    class InputValidator
+      attr_reader :callable
+
+      def initialize(callable)
+        @callable = callable
+      end
+
+      def call(validatable)
+        error = @callable.call(validatable.input)
+        validatable.add_error(error) if error
+      end
+    end
+  end
+end
