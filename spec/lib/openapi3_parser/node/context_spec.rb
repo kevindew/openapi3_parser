9ebddd886da5eba1b4b9ad2c1b780149134eb8ec
Kevin Dew <kevindew@me.com>
2019-10-04 23:53:03 +0100

    Add some tests for Node::Context
    This is definitely not my finest hour as these were super hard to
    understand to write, what monster have I created with all this context
    stuff?
    
    Alas, this provides unit tests for the static methods that hopefully
    will one day yield value.

---
+++ spec/lib/openapi3_parser/node/context_spec.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+require "support/helpers/context"
+require "support/helpers/source"
+
+RSpec.describe Openapi3Parser::Node::Context do
+  include Helpers::Context
+  include Helpers::Source
+
+  describe ".root" do
+    let(:factory_context) { create_node_factory_context({}) }
+
+    it "returns an instance of context" do
+      expect(described_class.root(factory_context))
+        .to be_a(described_class)
+    end
+
+    it "has a document location of root" do
+      context = described_class.root(factory_context)
+
+      expect(context.document_location.to_s).to eq "#/"
+    end
+  end
+
+  describe ".next_field" do
+    subject(:context) do
+      described_class.next_field(parent_context, field, factory_context)
+    end
+
+    let(:parent_context) { create_node_context({}) }
+    let(:factory_context) { create_node_factory_context({}) }
+    let(:field) { "key" }
+
+    it "has a document location of '#/key'" do
+      expect(context.document_location.to_s).to eq "#/key"
+    end
+  end
+
+  describe ".resolved_reference" do
+    subject(:context) do
+      described_class.resolved_reference(current_context,
+                                         reference_factory_context)
+    end
+
+    let(:current_context) do
+      create_node_context({}, pointer_segments: %w[field])
+    end
+
+    let(:reference_factory_context) do
+      source_location = create_source_location(
+        {},
+        document: current_context.document,
+        pointer_segments: %w[data]
+      )
+
+      reference_location = create_source_location(
+        {},
+        document: current_context.document,
+        pointer_segments: %w[field $ref]
+      )
+
+      Openapi3Parser::NodeFactory::Context.new(
+        "data",
+        source_location: source_location,
+        reference_locations: [reference_location]
+      )
+    end
+
+    it "has the referenced input" do
+      expect(context.input).to eq "data"
+    end
+
+    it "maintains the document location" do
+      expect(context.document_location.to_s)
+        .to eq "#/field"
+    end
+
+    it "knows the source location is where the referenced data is" do
+      expect(context.source_location.to_s)
+        .to eq "#/data"
+    end
+
+    it "knows the location of the reference" do
+      expect(context.reference_location.to_s)
+        .to eq "#/field/%24ref"
+    end
+  end
+end
