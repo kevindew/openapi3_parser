09b678df80323701f5d2e4b400a3b35c0d633a14
Kevin Dew <kevindew@me.com>
2019-10-07 21:32:09 +0100

    Fix wrong name and content test
    I had to try take my brain back to July 2018 to try remember what I
    wrote in this test originally and never managed to commit.

---
+++ spec/integration/open_a_document_with_defaults_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+RSpec.describe "Open a document with defaults" do
+  subject(:document) { Openapi3Parser.load(input) }
+
+  let(:input) do
+    {
+      openapi: "3.0.0",
+      info: {
+        title: "Test Document",
+        version: "1.0.0"
+      },
+      paths: {
+        "/path": {
+          get: {
+            responses: {
+              default: {
+                description: "Get response",
+                content: {
+                  "application/json": {
+                    example: "test"
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      components: {
+        schemas: {
+          my_schema: {
+            title: "My Schema"
+          }
+        }
+      }
+    }
+  end
+
+  it { is_expected.to be_valid }
+
+  it "has nil values for objects without defaults" do
+    expect(document.info.contact).to be_nil
+  end
+
+  it "has nil values for arrays that don't need a value" do
+    expect(document.components.schemas["my_schema"].required).to be_nil
+  end
+
+  it "has nil values for objects that default to nil" do
+    media_type = document.paths["/path"]
+                         .get
+                         .responses["default"]
+                         .content["application/json"]
+    expect(media_type.examples).to be_nil
+  end
+end
